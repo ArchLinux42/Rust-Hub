90039bbd92c66a4cf42571a8783808c86fc11eeb
ArchLinux42 <103643956+ArchLinux42@users.noreply.github.com>
2022-04-15 03:44:37 -0400

    Update script.lua
    second page + new feature

--- script.lua
+++ script.lua
@@ -1,103 +1,78 @@
--- Made by Arch
--- Fuck you Piixel
+--// Made by Arch
+--// Fuck you Piixel
 -- Gui to Lua
 -- Version: 3.2
 
 -- Instances:
 
-local ScreenGui = Instance.new("ScreenGui")
 local Drag = Instance.new("Frame")
-local Main = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
-local OwlHub = Instance.new("TextButton")
-local UICorner_2 = Instance.new("UICorner")
-local CMDX = Instance.new("TextButton")
-local UICorner_3 = Instance.new("UICorner")
-local InfiniteYiff = Instance.new("TextButton")
-local UICorner_4 = Instance.new("UICorner")
-local OpenwithRightShift = Instance.new("TextLabel")
-local UICorner_5 = Instance.new("UICorner")
-local Close = Instance.new("TextButton")
+local Scripts = Instance.new("Folder")
 local RustHub = Instance.new("TextLabel")
+local UICorner_2 = Instance.new("UICorner")
+local Close = Instance.new("TextButton")
+local Main = Instance.new("Frame")
+local UICorner_3 = Instance.new("UICorner")
+local OpenwithRightShift = Instance.new("TextLabel")
+local CMDX = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local OwlHub = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local InfiniteYiff = Instance.new("TextButton")
 local UICorner_6 = Instance.new("UICorner")
+local ForMoreinfo = Instance.new("TextLabel")
+local Right = Instance.new("TextButton")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local Second = Instance.new("Frame")
+local UICorner_7 = Instance.new("UICorner")
+local OpenwithRightShift_2 = Instance.new("TextLabel")
+local Orca = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
+local Dex = Instance.new("TextButton")
+local UICorner_9 = Instance.new("UICorner")
+local T3PHub = Instance.new("TextButton")
+local UICorner_10 = Instance.new("UICorner")
+local ForMoreinfo_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+local Left = Instance.new("TextButton")
+local Info = Instance.new("Frame")
+local UICorner_11 = Instance.new("UICorner")
+local DEX = Instance.new("TextLabel")
+local Back = Instance.new("TextButton")
+local Yiff = Instance.new("TextLabel")
+local CMD = Instance.new("TextLabel")
 
 --Properties:
 
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
 Drag.Name = "Drag"
-Drag.Parent = ScreenGui
+Drag.Parent = game.StarterGui.ScreenGui
 Drag.AnchorPoint = Vector2.new(0.5, 0.5)
 Drag.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
-Drag.Position = UDim2.new(0.496228784, 0, 0.200000003, 0)
-Drag.Size = UDim2.new(0, 517, 0, 80)
+Drag.Position = UDim2.new(0.511313677, 0, 0.265644163, 0)
+Drag.Size = UDim2.new(0, 517, 0, 79)
 
-Main.Name = "Main"
-Main.Parent = Drag
-Main.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
-Main.Position = UDim2.new(0, 0, 0.481888592, 0)
-Main.Size = UDim2.new(0, 517, 0, 381)
-Main.ZIndex = 0
+UICorner.Parent = Drag
 
-UICorner.CornerRadius = UDim.new(0.100000001, 0)
-UICorner.Parent = Main
+Scripts.Name = "Scripts"
+Scripts.Parent = Drag
 
-OwlHub.Name = "OwlHub"
-OwlHub.Parent = Main
-OwlHub.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
-OwlHub.Position = UDim2.new(0.23210831, 0, 0.107611552, 0)
-OwlHub.Size = UDim2.new(0, 277, 0, 62)
-OwlHub.Font = Enum.Font.FredokaOne
-OwlHub.Text = "Owl Hub"
-OwlHub.TextColor3 = Color3.fromRGB(255, 255, 255)
-OwlHub.TextSize = 36.000
+RustHub.Name = "Rust Hub"
+RustHub.Parent = Scripts
+RustHub.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
+RustHub.Size = UDim2.new(0, 517, 0, 39)
+RustHub.ZIndex = 0
+RustHub.Font = Enum.Font.FredokaOne
+RustHub.Text = "Rust Hub"
+RustHub.TextColor3 = Color3.fromRGB(255, 255, 255)
+RustHub.TextScaled = true
+RustHub.TextSize = 14.000
+RustHub.TextWrapped = true
 
-UICorner_2.CornerRadius = UDim.new(1, 1)
-UICorner_2.Parent = OwlHub
-
-CMDX.Name = "CMD-X"
-CMDX.Parent = Main
-CMDX.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
-CMDX.Position = UDim2.new(0.23210831, 0, 0.377952754, 0)
-CMDX.Size = UDim2.new(0, 277, 0, 62)
-CMDX.Font = Enum.Font.FredokaOne
-CMDX.Text = "CMD-X"
-CMDX.TextColor3 = Color3.fromRGB(255, 255, 255)
-CMDX.TextSize = 36.000
-
-UICorner_3.CornerRadius = UDim.new(1, 1)
-UICorner_3.Parent = CMDX
-
-InfiniteYiff.Name = "Infinite Yiff"
-InfiniteYiff.Parent = Main
-InfiniteYiff.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
-InfiniteYiff.Position = UDim2.new(0.23210831, 0, 0.648293972, 0)
-InfiniteYiff.Size = UDim2.new(0, 277, 0, 62)
-InfiniteYiff.Font = Enum.Font.FredokaOne
-InfiniteYiff.Text = "Infinite Yield"
-InfiniteYiff.TextColor3 = Color3.fromRGB(255, 255, 255)
-InfiniteYiff.TextSize = 36.000
-
-UICorner_4.CornerRadius = UDim.new(1, 1)
-UICorner_4.Parent = InfiniteYiff
-
-OpenwithRightShift.Name = "Open with Right Shift"
-OpenwithRightShift.Parent = Main
-OpenwithRightShift.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-OpenwithRightShift.BackgroundTransparency = 1.000
-OpenwithRightShift.Position = UDim2.new(0, 0, 0.868766427, 0)
-OpenwithRightShift.Size = UDim2.new(0, 517, 0, 50)
-OpenwithRightShift.Font = Enum.Font.FredokaOne
-OpenwithRightShift.Text = "Open GUI with Right  Shift                                          Made by Arch"
-OpenwithRightShift.TextColor3 = Color3.fromRGB(255, 255, 255)
-OpenwithRightShift.TextSize = 20.000
-OpenwithRightShift.TextWrapped = true
-
-UICorner_5.Parent = Drag
+UICorner_2.Parent = RustHub
 
 Close.Name = "Close"
-Close.Parent = Drag
+Close.Parent = Scripts
 Close.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Close.BackgroundTransparency = 1.000
 Close.BorderColor3 = Color3.fromRGB(27, 42, 53)
@@ -111,35 +86,287 @@ Close.TextScaled = true
 Close.TextSize = 14.000
 Close.TextWrapped = true
 
-RustHub.Name = "Rust Hub"
-RustHub.Parent = Drag
-RustHub.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
-RustHub.Size = UDim2.new(0, 517, 0, 39)
-RustHub.ZIndex = 0
-RustHub.Font = Enum.Font.FredokaOne
-RustHub.Text = "Rust Hub"
-RustHub.TextColor3 = Color3.fromRGB(255, 255, 255)
-RustHub.TextScaled = true
-RustHub.TextSize = 14.000
-RustHub.TextWrapped = true
+Main.Name = "Main"
+Main.Parent = Drag
+Main.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+Main.Position = UDim2.new(0, 0, 0.501710534, 0)
+Main.Size = UDim2.new(0, 517, 0, 388)
+Main.ZIndex = 0
 
-UICorner_6.Parent = RustHub
+UICorner_3.CornerRadius = UDim.new(0.100000001, 0)
+UICorner_3.Parent = Main
+
+OpenwithRightShift.Name = "Open with Right Shift"
+OpenwithRightShift.Parent = Main
+OpenwithRightShift.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+OpenwithRightShift.BackgroundTransparency = 1.000
+OpenwithRightShift.Position = UDim2.new(0, 0, 0.868766427, 0)
+OpenwithRightShift.Size = UDim2.new(0, 517, 0, 50)
+OpenwithRightShift.Font = Enum.Font.FredokaOne
+OpenwithRightShift.Text = "Open GUI with Right  Shift                                          Made by Arch"
+OpenwithRightShift.TextColor3 = Color3.fromRGB(255, 255, 255)
+OpenwithRightShift.TextSize = 20.000
+OpenwithRightShift.TextWrapped = true
+
+CMDX.Name = "CMD-X"
+CMDX.Parent = Main
+CMDX.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
+CMDX.Position = UDim2.new(0.23210831, 0, 0.37537545, 0)
+CMDX.Size = UDim2.new(0, 277, 0, 62)
+CMDX.Font = Enum.Font.FredokaOne
+CMDX.Text = "CMD-X"
+CMDX.TextColor3 = Color3.fromRGB(255, 255, 255)
+CMDX.TextSize = 36.000
+
+UICorner_4.CornerRadius = UDim.new(1, 1)
+UICorner_4.Parent = CMDX
+
+OwlHub.Name = "OwlHub"
+OwlHub.Parent = Main
+OwlHub.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
+OwlHub.Position = UDim2.new(0.23210831, 0, 0.107611552, 0)
+OwlHub.Size = UDim2.new(0, 277, 0, 62)
+OwlHub.Font = Enum.Font.FredokaOne
+OwlHub.Text = "Owl Hub"
+OwlHub.TextColor3 = Color3.fromRGB(255, 255, 255)
+OwlHub.TextSize = 36.000
+
+UICorner_5.CornerRadius = UDim.new(1, 1)
+UICorner_5.Parent = OwlHub
+
+InfiniteYiff.Name = "Infinite Yiff"
+InfiniteYiff.Parent = Main
+InfiniteYiff.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
+InfiniteYiff.Position = UDim2.new(0.23210831, 0, 0.648293972, 0)
+InfiniteYiff.Size = UDim2.new(0, 277, 0, 62)
+InfiniteYiff.Font = Enum.Font.FredokaOne
+InfiniteYiff.Text = "Infinite Yield"
+InfiniteYiff.TextColor3 = Color3.fromRGB(255, 255, 255)
+InfiniteYiff.TextSize = 36.000
+
+UICorner_6.CornerRadius = UDim.new(1, 1)
+UICorner_6.Parent = InfiniteYiff
+
+ForMoreinfo.Name = "For More info"
+ForMoreinfo.Parent = Main
+ForMoreinfo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ForMoreinfo.BackgroundTransparency = 1.000
+ForMoreinfo.Position = UDim2.new(0.023210831, 0, 0, 0)
+ForMoreinfo.Size = UDim2.new(0, 229, 0, 40)
+ForMoreinfo.Font = Enum.Font.FredokaOne
+ForMoreinfo.Text = "If * next to script right click for more info"
+ForMoreinfo.TextColor3 = Color3.fromRGB(255, 255, 255)
+ForMoreinfo.TextSize = 20.000
+ForMoreinfo.TextXAlignment = Enum.TextXAlignment.Left
+
+Right.Name = "Right"
+Right.Parent = Main
+Right.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Right.BackgroundTransparency = 1.000
+Right.Position = UDim2.new(0.876208961, 0, -0.00257731974, 0)
+Right.Size = UDim2.new(0, 64, 0, 388)
+Right.Font = Enum.Font.FredokaOne
+Right.Text = ">"
+Right.TextColor3 = Color3.fromRGB(255, 255, 255)
+Right.TextSize = 68.000
+
+TextLabel.Name = "*"
+TextLabel.Parent = Main
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.700193405, 0, 0.536082506, 0)
+TextLabel.Size = UDim2.new(0, 35, 0, 0)
+TextLabel.Font = Enum.Font.FredokaOne
+TextLabel.Text = "*"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 72.000
+TextLabel.TextWrapped = true
+
+TextLabel_2.Name = "*"
+TextLabel_2.Parent = Main
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.Position = UDim2.new(0.700193405, 0, 0.806701064, 0)
+TextLabel_2.Size = UDim2.new(0, 35, 0, 0)
+TextLabel_2.Font = Enum.Font.FredokaOne
+TextLabel_2.Text = "*"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextSize = 72.000
+TextLabel_2.TextWrapped = true
+
+Second.Name = "Second"
+Second.Parent = Drag
+Second.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+Second.Position = UDim2.new(0, 0, 0.489052296, 0)
+Second.Size = UDim2.new(0, 517, 0, 388)
+Second.Visible = false
+Second.ZIndex = 0
+
+UICorner_7.CornerRadius = UDim.new(0.100000001, 0)
+UICorner_7.Parent = Second
+
+OpenwithRightShift_2.Name = "Open with Right Shift"
+OpenwithRightShift_2.Parent = Second
+OpenwithRightShift_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+OpenwithRightShift_2.BackgroundTransparency = 1.000
+OpenwithRightShift_2.Position = UDim2.new(0, 0, 0.868766427, 0)
+OpenwithRightShift_2.Size = UDim2.new(0, 517, 0, 50)
+OpenwithRightShift_2.Font = Enum.Font.FredokaOne
+OpenwithRightShift_2.Text = "Open GUI with Right  Shift                                          Made by Arch"
+OpenwithRightShift_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+OpenwithRightShift_2.TextSize = 20.000
+OpenwithRightShift_2.TextWrapped = true
+
+Orca.Name = "Orca"
+Orca.Parent = Second
+Orca.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
+Orca.Position = UDim2.new(0.23210831, 0, 0.377952754, 0)
+Orca.Size = UDim2.new(0, 277, 0, 62)
+Orca.Font = Enum.Font.FredokaOne
+Orca.Text = "Orca"
+Orca.TextColor3 = Color3.fromRGB(255, 255, 255)
+Orca.TextSize = 36.000
+
+UICorner_8.CornerRadius = UDim.new(1, 1)
+UICorner_8.Parent = Orca
+
+Dex.Name = "Dex"
+Dex.Parent = Second
+Dex.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
+Dex.Position = UDim2.new(0.23210831, 0, 0.107611552, 0)
+Dex.Size = UDim2.new(0, 277, 0, 62)
+Dex.Font = Enum.Font.FredokaOne
+Dex.Text = "DEX"
+Dex.TextColor3 = Color3.fromRGB(255, 255, 255)
+Dex.TextSize = 36.000
+
+UICorner_9.CornerRadius = UDim.new(1, 1)
+UICorner_9.Parent = Dex
+
+T3PHub.Name = "T3PHub"
+T3PHub.Parent = Second
+T3PHub.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
+T3PHub.Position = UDim2.new(0.23210831, 0, 0.648293972, 0)
+T3PHub.Size = UDim2.new(0, 277, 0, 62)
+T3PHub.Font = Enum.Font.FredokaOne
+T3PHub.Text = "T3PHub"
+T3PHub.TextColor3 = Color3.fromRGB(255, 255, 255)
+T3PHub.TextSize = 36.000
+
+UICorner_10.CornerRadius = UDim.new(1, 1)
+UICorner_10.Parent = T3PHub
+
+ForMoreinfo_2.Name = "For More info"
+ForMoreinfo_2.Parent = Second
+ForMoreinfo_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ForMoreinfo_2.BackgroundTransparency = 1.000
+ForMoreinfo_2.Position = UDim2.new(0.023210831, 0, 0, 0)
+ForMoreinfo_2.Size = UDim2.new(0, 229, 0, 40)
+ForMoreinfo_2.Font = Enum.Font.FredokaOne
+ForMoreinfo_2.Text = "If * next to script right click for more info"
+ForMoreinfo_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+ForMoreinfo_2.TextSize = 20.000
+ForMoreinfo_2.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_3.Name = "*"
+TextLabel_3.Parent = Second
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.Position = UDim2.new(0.700193405, 0, 0.265463918, 0)
+TextLabel_3.Size = UDim2.new(0, 35, 0, 0)
+TextLabel_3.Font = Enum.Font.FredokaOne
+TextLabel_3.Text = "*"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.TextSize = 72.000
+TextLabel_3.TextWrapped = true
+
+Left.Name = "Left"
+Left.Parent = Second
+Left.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Left.BackgroundTransparency = 1.000
+Left.Position = UDim2.new(0, 0, -0.00257731951, 0)
+Left.Size = UDim2.new(0, 64, 0, 388)
+Left.Font = Enum.Font.FredokaOne
+Left.Text = "<"
+Left.TextColor3 = Color3.fromRGB(255, 255, 255)
+Left.TextSize = 68.000
+
+Info.Name = "Info"
+Info.Parent = Drag
+Info.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+Info.Position = UDim2.new(0, 0, 0.527027011, 0)
+Info.Size = UDim2.new(0, 517, 0, 388)
+Info.Visible = false
+Info.ZIndex = 0
+
+UICorner_11.CornerRadius = UDim.new(0.100000001, 0)
+UICorner_11.Parent = Info
+
+DEX.Name = "DEX"
+DEX.Parent = Info
+DEX.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+DEX.BackgroundTransparency = 1.000
+DEX.Size = UDim2.new(0, 517, 0, 388)
+DEX.Visible = false
+DEX.Font = Enum.Font.FredokaOne
+DEX.Text = "DEX - Client sided file manager, although very limited"
+DEX.TextColor3 = Color3.fromRGB(255, 255, 255)
+DEX.TextSize = 63.000
+DEX.TextWrapped = true
+
+Back.Name = "Back"
+Back.Parent = Info
+Back.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Back.BackgroundTransparency = 1.000
+Back.Position = UDim2.new(0, 0, 0.79896909, 0)
+Back.Size = UDim2.new(0, 80, 0, 77)
+Back.ZIndex = 2
+Back.Font = Enum.Font.FredokaOne
+Back.Text = "<"
+Back.TextColor3 = Color3.fromRGB(255, 255, 255)
+Back.TextSize = 68.000
+
+Yiff.Name = "Yiff"
+Yiff.Parent = Info
+Yiff.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Yiff.BackgroundTransparency = 1.000
+Yiff.Size = UDim2.new(0, 517, 0, 388)
+Yiff.Visible = false
+Yiff.Font = Enum.Font.FredokaOne
+Yiff.Text = "Infinite Yield - Admin commands (fly, goto, etc.) Most commands cant be used on other players"
+Yiff.TextColor3 = Color3.fromRGB(255, 255, 255)
+Yiff.TextSize = 63.000
+Yiff.TextWrapped = true
+
+CMD.Name = "CMD"
+CMD.Parent = Info
+CMD.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+CMD.BackgroundTransparency = 1.000
+CMD.Size = UDim2.new(0, 517, 0, 388)
+CMD.Visible = false
+CMD.Font = Enum.Font.FredokaOne
+CMD.Text = "CMD-X - Admin commands (fly, goto, etc.) Most commands cant be used on other players."
+CMD.TextColor3 = Color3.fromRGB(255, 255, 255)
+CMD.TextSize = 63.000
+CMD.TextWrapped = true
 
 -- Scripts:
 
-local function RHLK_fake_script() -- Drag.Handler 
-	local script = Instance.new('LocalScript', Drag)
+local function IHAULI_fake_script() -- Scripts.Handler 
+	local script = Instance.new('LocalScript', Scripts)
 
 	--//Dragable Dont Edit
 	
 	local UserInputService = game:GetService("UserInputService")
 	
-	local gui = script.Parent
+	local gui = script.Parent.Parent
 	local toggle = true
 	local dragging
 	local dragInput
 	local dragStart
 	local startPos
+	local info = script.Parent.Parent.Info
+	local cur = script.Parent.Parent.Main
 	
 	local function update(input)
 		local delta = input.Position - dragStart
@@ -191,24 +418,95 @@ local function RHLK_fake_script() -- Drag.Handler
 		end
 	end)
 	
+	--//Pages
+	local right = gui.Main.Right
+	local left = gui.Second.Left
+	right.MouseButton1Click:Connect(function()
+		right.Parent.Visible = false
+		left.Parent.Visible = true
+	end)
+	left.MouseButton1Click:Connect(function()
+		left.Parent.Visible = false
+		right.Parent.Visible = true
+	end)
+	
+	
 	--//Owl Hub
-	local Owl = script.Parent.Main.OwlHub
+	local Owl = script.Parent.Parent.Main.OwlHub
 	Owl.MouseButton1Click:Connect(function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
 	end)
+	Owl.MouseButton2Click:Connect(function()
+		cur = gui.Main
+		gui.Main.Visible = false
+		gui.Second.Visible = false
+		gui.Info.Visible = true
+		gui.Info.CMD.Visible = true
+	end)
+	
 	
 	--//CMD-X
-	local CMD = script.Parent.Main["CMD-X"]
+	local CMD = script.Parent.Parent.Main["CMD-X"]
 	CMD.MouseButton1Click:Connect(function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
 	end)
+	CMD.MouseButton2Click:Connect(function()
+		cur = gui.Main
+		gui.Main.Visible = false
+		gui.Second.Visible = false
+		gui.Info.Visible = true
+		gui.Info.CMD.Visible = true
+	end)
 	
 	--//Infinite Yiff
-	local Inf = script.Parent.Main["Infinite Yiff"]
+	local Inf = script.Parent.Parent.Main["Infinite Yiff"]
 	Inf.MouseButton1Click:Connect(function()
 		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 	end)
+	Inf.MouseButton2Click:Connect(function()
+		cur = gui.Main
+		gui.Main.Visible = false
+		gui.Second.Visible = false
+		gui.Info.Visible = true
+		gui.Info.CMD.Visible = true
+	end)
 	
-	--//More??
+	
+	--//DEX
+	local DEX = script.Parent.Parent.Second.Dex
+	local main = script.Parent.Parent.Main
+	local second = script.Parent.Parent.Second
+	DEX.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
+	end)
+	DEX.MouseButton2Click:Connect(function()
+		cur = second
+		main.Visible = false
+		second.Visible = false
+		info.Visible = true
+		info.DEX.Visible = true
+	end)
+	
+	--//Info back
+	local back = info.Back
+	back.MouseButton1Click:Connect(function()
+		cur.Visible = true
+		info.Visible = false
+	end)
+	
+	--//T3PHub
+	local T3P = gui.Second.T3PHub
+	T3P.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/MegamiShin/Exploit-Scripts/main/T3PHUB/Script.lua", true))()
+	end)
+	
+	--//Orca
+	local Orca = gui.Second.Orca
+	Orca.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua"))()
+	end)
+	Orca.MouseButton2Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/snapshot.lua"))()
+	end)
 end
-coroutine.wrap(RHLK_fake_script)()
+coroutine.wrap(IHAULI_fake_script)()
